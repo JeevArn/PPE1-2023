65616c25c0a6764c56afddd69264cfd36940d77b
Jeevya Aroun <jeevyaroun@MacBook-Pro-de-Jeevya-2.local>
2023-10-16 22:37:51 +0200

    modif exo script Shell

--- scriptEx1diapo29_step2.sh
+++ scriptEx1diapo29_step2.sh
@@ -2,6 +2,8 @@
 
 #Écrire un second script qui lance le script précédent trois fois, une fois pour chaque années, en prenant le type d’entité en argument.
 
+chmod +x scriptEx1diapo29.sh
+
 typeEntite=$1
 ./scriptEx1diapo29.sh 2016 "$1"
 ./scriptEx1diapo29.sh 2017 "$1"
